df3e93ae08930e2b06f88e28e42d00fd2927e805
Kartik2005 <kartikthakursaklani@gmail.com>
2024-10-21 19:05:48 +0530

    interfaces

---
+++ interfaces/calculator.java
@@ -0,0 +1,5 @@
+public interface calculator 
+{
+     public int add(int a, int b);
+     public int sub(int a, int c);    
+}
---
+++ interfaces/try_calc.java
@@ -0,0 +1,8 @@
+public class try_calc
+{
+    public static void main(String[] args) 
+    {
+        calc abc = new calc();
+        System.out.println(abc.add(1, 2));   
+    }
+}
